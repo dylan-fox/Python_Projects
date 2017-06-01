6488c27303de67ee29a56cbfa6bc9463d43a3cfd
Dylan Fox <dylan-fox@users.noreply.github.com>
2017-06-01 10:18:52 -0700

    Hard Way exercises 1-10

---
+++ hardway/ex3-1.py
@@ -0,0 +1,16 @@
+print 12/1.0
+print 12/2.0
+print 12/3.0
+print 12/4.0
+print 12/5.0
+print 12/6.0
+print 12/7.0
+print 12/8.0
+print 12/9.0
+print 12/10.0
+print 12/11.0
+print 12/12.0
+
+print "Here's the deal:"
+print 7.0/4.0
+print 7/4
